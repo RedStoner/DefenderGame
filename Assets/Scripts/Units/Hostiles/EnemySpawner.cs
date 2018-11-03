a4a3fabf68fc64a5c8b141cb68dda7b7d44fe70d
RedStoner <the3vilm0nk3y@gmail.com>
2018-11-03 16:39:21 -0400

    More Hostiles per second
    Make hostiles spawn every set amount of seconds. Lowered amount to spawn to 4.

--- Assets/Scripts/Units/Hostiles/EnemySpawner.cs
+++ Assets/Scripts/Units/Hostiles/EnemySpawner.cs
@@ -6,25 +6,44 @@ public class EnemySpawner : NetworkBehaviour
 
     public GameObject enemyPrefab;
     public int numberOfEnemies;
+    public float spawnInterval = 10.0f;
+    private float spawnCooldown = 0.0f;
 
     public override void OnStartServer()
     {
         if (isServer) {
-            for (int i = 0; i < numberOfEnemies; i++)
+            SpawnEnemies();
+        }
+    }
+    private void Update()
+    {
+        if (isServer)
+        {
+            spawnCooldown += Time.deltaTime;
+            if (spawnCooldown > spawnInterval)
             {
-                var spawnPosition = new Vector3(
-                    Random.Range(0.0f, 0.5f),
-                    0.0f,
-                    Random.Range(0.0f, 0.5f));
-
-                var spawnRotation = Quaternion.Euler(
-                    0.0f,
-                    Random.Range(0, 180),
-                    0.0f);
-
-                var enemy = (GameObject)Instantiate(enemyPrefab, spawnPosition, spawnRotation);
-                NetworkServer.Spawn(enemy);
+                SpawnEnemies();
+                spawnCooldown -= spawnInterval;
             }
+        }
+    }
+    private void SpawnEnemies()
+    {
+
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            var spawnPosition = new Vector3(
+                Random.Range(0.0f, 0.5f),
+                0.0f,
+                Random.Range(0.0f, 0.5f));
+
+            var spawnRotation = Quaternion.Euler(
+                0.0f,
+                Random.Range(0, 180),
+                0.0f);
+
+            var enemy = (GameObject)Instantiate(enemyPrefab, spawnPosition, spawnRotation);
+            NetworkServer.Spawn(enemy);
         }
     }
 }
